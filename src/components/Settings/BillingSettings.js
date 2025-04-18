78d10f8e8ef90beac801e8e5bfe322ad580ef853
Ritik Goyal <ritik.goyal@cogoport.com>
2025-04-19 01:44:56 +0530

    assets removed

--- src/components/Settings/BillingSettings.js
+++ src/components/Settings/BillingSettings.js
@@ -56,36 +56,6 @@ const BillingSettings = async () => {
         </Modal>
       </div>
 
-      {/* <div className="lg:col-span-2 flex justify-start lg:justify-center ">
-        <Modal
-          title="Choose A Plan"
-          description="Tell us about yourself! What do you do? Let’s tailor your experience so it best suits you."
-          trigger={
-            plan && plan === 'STANDARD' ? (
-              <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
-                <CardContent className="flex gap-2 items-center">
-                  <div className="rounded-full border-2 p-1">
-                    <Plus className="text-gray-400" />
-                  </div>
-                  <CardDescription className="font-semibold">
-                    Upgrade Plan
-                  </CardDescription>
-                </CardContent>
-              </Card>
-            ) : (
-              <Image
-                src="/images/creditcard.png"
-                width={400}
-                height={400}
-                alt="image"
-              />
-            )
-          }
-        >
-          <SubscriptionForm plan={plan!} />
-        </Modal>
-      </div> */}
-
       <div className="lg:col-span-2">
         <h3 className="text-md mb-2">Current Plan</h3>
 
