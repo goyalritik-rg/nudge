c3157070cce9d33610fb9ff2ef90c4cc5ffccb3e
Ritik Goyal <ritik.goyal@cogoport.com>
2025-04-19 01:37:07 +0530

    css fixes

--- src/components/Chatbot/RealTimeMode.js
+++ src/components/Chatbot/RealTimeMode.js
@@ -2,7 +2,7 @@ import { Card } from "../ui/card";
 
 const RealTimeMode = () => {
   return (
-    <Card className="px-3 rounded-full py-1 bg-orange-400 font-semibold text-white text-sm mt-1">
+    <Card className="px-3 rounded-full py-1 bg-orange-400 font-semibold text-white text-xs mt-1">
       Real Time
     </Card>
   );
